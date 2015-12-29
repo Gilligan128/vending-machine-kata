3046558bdf923dd4df0b998ef8f6d04fc1e14c1e
Mark Henke <gilligan128@gmail.com>
2015-12-29 11:12:46 -0500

    first passing test for default display

---
+++ PillarKata.VendingMachine/PillarKata.VendingMachine/VendingMachine.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PillarKata.VendingMachine
+{
+    public class VendingMachine
+    {
+        public Display CheckDisplay()
+        {
+            return new Display("INSERT COINS");
+        }
+
+        public class Display
+        {
+            public string Message { get; private set; }
+
+            public Display(string message)
+            {
+                Message = message;
+            }
+        }
+    }
+
+    
+}
